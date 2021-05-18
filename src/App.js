24b4b21711c2f4714438516bc9b480a8c3096ec2
Gabriel <gabrielfonsecaoficial@gmail.com>
2021-05-18 18:04:46 -0300

    usando rotas com Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React, { useState } from 'react'
-import Modal from './components/Modal'
+import React from 'react'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
 
 const App = () => {
-
-  const [modalVisible, setModalVisible] = useState(false)
-
-  const handleButtonClick = () => setModalVisible(true)
-
   return (
-    <>
-      <button onClick={handleButtonClick}>Abrir Modal</button>
-      <Modal visible={modalVisible} setVisible={setModalVisible}>
-        <h1>Este aqui será o Modal</h1>
-      </Modal>
-    </>
+    <BrowserRouter>
+      <header>
+        <h1>Meu site legal</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/sobre">Sobre</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <Switch>
+        <Route exact path="/"><Home /></Route>
+        <Route path="/sobre"><Sobre /></Route>
+      </Switch>
+
+      <hr />
+      <footer>Todos os direitos reservados... </footer>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Sobre.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sobre = () => {
+    return (
+        <div>HOME</div>
+    )
+}
+
+export default Sobre
