087125602e037ca3548acfea2ba7bd13c32d4dc1
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-05-20 16:49:36 -0300

    Redux: Connect e Dispatch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Home = () => {
+const Home = (props) => {
 
     let history = useHistory()
     const handleButton = () => {
@@ -10,13 +11,50 @@ const Home = () => {
         }, 2000)
     }
 
+    const handleBonieky = () => {
+        props.setName('Bonieky')
+    }
+
+    const handleIncrement = () => {
+        props.increment()
+    }
+
     return (
         <div>
             <h4>PÁGINA HOME</h4>
+
+            NOME: {props.name} <br />
+            CONTAGEM: {props.contador}
+
+            <button onClick={handleBonieky}>Setar nome para Bonieky</button>
+            <button onClick={handleIncrement}>+1</button>
+
+            <br />
+            <br />
 
             <button onClick={handleButton} >Ir para a página SOBRE</button>
         </div>
     )
 }
 
-export default Home
+const mapStateToProps = (state) => {
+    return {
+        name: state.usuario.name,
+        contator: state.usuario.contador
+    }
+}
+
+const mapDistatchToProps = (dispatch) => {
+    return {
+        setName: (newName) => dispatch({
+            type: 'SET_NAME',
+            payload: { name: newName }
+        }),
+
+        increment: () => dispatch({
+            type: 'INCREMENT_CONTADOR'
+        })
+    }
+}
+
+export default connect(mapStateToProps, mapDistatchToProps)(Home);
